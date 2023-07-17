9eaa6803a5163154d5a90046f227575cf18294ab
Daniel Amaya <danijcdm.com@gmail.com>
2023-07-17 13:39:27 -0500

    Realizar migraciones requeridas

---
+++ database/migrations/2023_07_15_184430_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 30);
+
+            $table->bigInteger('proveedor_id')->unsigned();
+            $table->bigInteger('categoria_id')->unsigned();
+            
+           
+            $table->foreign('proveedor_id')->references('id')->on('proveedores')->onDelete('cascade');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
